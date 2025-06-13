34267a6e6e501833f7328f867c5c99c372e5feef
doston1002 <polatovdoston1002@gmail.com>
2025-06-13 17:21:15 +0500

    commit

--- src/pages/articles/index.tsx
+++ src/pages/articles/index.tsx
@@ -1,11 +1,8 @@
-import { GetServerSideProps } from 'next';
 import { useTranslation } from 'react-i18next';
 import { ArticleType } from 'src/interfaces/article.interface';
-import { Language } from 'src/interfaces/constants.interface';
 import { withLayout } from 'src/layouts/layout';
 import Seo from 'src/layouts/seo/seo';
 import { ArticlePageComponent } from 'src/page-component';
-import { FC } from 'react';
 
 interface ArticlesPageProps extends Record<string, unknown> {
 	articles: ArticleType;
