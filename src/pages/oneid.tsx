91d98a8bd9fddc477f7fc2c8a234755971189620
doston1002 <polatovdoston1002@gmail.com>
2025-08-27 08:28:07 +0500

    oneid page

---
+++ src/pages/oneid.tsx
@@ -0,0 +1,25 @@
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+
+export default function OneIdPage() {
+  const router = useRouter()
+
+  useEffect(() => {
+    if (router.isReady) {
+      const { code, state } = router.query
+
+      console.log("OneID qaytdi:", code, state)
+
+      // agar backend orqali ishlashni xohlasangiz:
+      // fetch("/api/your-backend-route?code=" + code)
+
+    }
+  }, [router.isReady, router.query])
+
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>OneID Callback Page</h1>
+      <p>Agar code keldi: brauzer konsolini ochib tekshiring.</p>
+    </div>
+  )
+}
