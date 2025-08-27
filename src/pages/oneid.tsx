17da5d5c110a15f03acae70b0ff0fe861f519b12
doston1002 <polatovdoston1002@gmail.com>
2025-08-27 08:45:04 +0500

    dd

--- src/pages/oneid.tsx
+++ src/pages/oneid.tsx
@@ -1,5 +1,6 @@
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
+import { API_URL } from 'src/config/api.config'
 
 export default function OneIdPage() {
   const router = useRouter()
@@ -15,6 +16,25 @@ export default function OneIdPage() {
 
     }
   }, [router.isReady, router.query])
+
+  useEffect(() => {
+    if (router.isReady) {
+      const { code, state } = router.query;
+  
+      if (code) {
+        fetch(`${API_URL}/OneId/OneIdApi?code=${code}&state=${state}`, {
+          credentials: "include", // cookie olish uchun
+        })
+          .then(res => {
+            if (res.redirected) {
+              window.location.href = res.url; // agar backend redirect qilsa
+            }
+          })
+          .catch(err => console.error(err));
+      }
+    }
+  }, [router.isReady, router.query]);
+  
 
   return (
     <div style={{ padding: 40 }}>
