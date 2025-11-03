342d6231613226ac6da38196e6e2980f7929df40
doston1002 <polatovdoston1002@gmail.com>
2025-11-03 22:33:43 +0500

    all

--- src/components/section-accordion/section-accordion.props.ts
+++ src/components/section-accordion/section-accordion.props.ts
@@ -3,7 +3,11 @@ import { SectionType } from 'src/interfaces/instructor.interface';
 
 export interface SectionAccordionProps {
 	section: SectionType;
-	setSectionTitle: Dispatch<SetStateAction<{ title: string; id: string } | null>>;
+    setSectionTitle: Dispatch<SetStateAction<{
+        title: string;
+        id: string;
+        firstLesson?: any | null;
+    } | null>>;
 	onOpen: () => void;
 	sectionIdx: number;
 }
